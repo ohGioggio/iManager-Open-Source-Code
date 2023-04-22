8f3267ac140a2fdacee3ff313a7683b5668b94f1
Gioele Allievi <68530313+ohGioggio@users.noreply.github.com>
2023-04-22 15:45:01 +0200

    Update advanced-security-algorithm.cs

--- security/advanced-security-algorithm.cs
+++ security/advanced-security-algorithm.cs
@@ -29,103 +29,97 @@ public class ConfigJson
 // Encrypt & Decrypt Class
 public class CryptClass {}
 
-public static string AdvancedSecurityAlgorithm()
+public static class SecurityChecker
 {
-    // Store Directory Paths
-    string[] dirlist = Directory.GetDirectories(Global.applicationfolderpath, "*", SearchOption.AllDirectories);
-    
-    // Initalize a List for the passwords
-    List<string> passwordlist = new();
-
-    // Store the most used passwords
-    string[] worstpasswords = { "123456", "123456789", "password", "qwerty", "12345678", "12345", "123123", "111111", "1234", "1234567890", "1234567", "abc123", "1q2w3e4r5t", "q1w2e3r4t5y6", "iloveyou", "123", "000000", "123321", "1q2w3e4r", "qwertyuiop", "654321", "qwerty123", "1qaz2wsx3edc", "password1", "1qaz2wsx", "666666", "dragon", "ashley", "princess", "987654321", "123qwe", "159753", "monkey", "q1w2e3r4", "zxcvbnm", "123123123", "asdfghjkl", "pokemon", "football", "killer", "112233", "michael", "shadow", "121212", "daniel", "asdasd", "qazwsx", "1234qwer", "superman", "123456a", "azerty", "qwe123", "master", "7777777", "sunshine", "N0=Acc3ss", "1q2w3e", "abcd1234", "1234561", "computer", "fuckyou ", "aaaaaa", "555555", "asdfgh", "asd123", "baseball", "0123456789", "charlie", "123654", "qwer1234", "naruto", "a123456", "jessica", "soccer", "jordan", "liverpool", "thomas", "lol123", "michelle", "123abc", "nicole", "11111111", "starwars", "samsung", "1111", "secret", "joshua", "123456789a", "andrew", "222222", "q1w2e3r4t5", "147258369", "hunter", "Password", "qazwsxedc", "lovely", "999999", "jennifer", "letmein", "tigger" };
-    
-    // Create Counters for the SecurityPage
-    int riskypasswordscount = 0;
-    int weakpasswordscount = 0;
-    int reusedpasswordscount = 0;
-    int safepasswordscount = 0;
-
-
-    // Skip the Folders for the list in Config
-    string json = File.ReadAllText(Global.configfilename);
-    ConfigJson config = JsonConvert.DeserializeObject<ConfigJson>(json);
-    List<string> list0 = new(dirlist);
-    if (config.SkipFolders != null)
+    public static string GetSecurityRate()
     {
-        foreach (string folder in config.SkipFolders)
+        var passwordList = new List<string>();
+
+        // Store the most used passwords
+        var worstPasswords = new[] { "123456", "123456789", "password", "qwerty", "12345678", "12345", "123123", "111111", "1234", "1234567890", "1234567", "abc123", "1q2w3e4r5t", "q1w2e3r4t5y6", "iloveyou", "123", "000000", "123321", "1q2w3e4r", "qwertyuiop", "654321", "qwerty123", "1qaz2wsx3edc", "password1", "1qaz2wsx", "666666", "dragon", "ashley", "princess", "987654321", "123qwe", "159753", "monkey", "q1w2e3r4", "zxcvbnm", "123123123", "asdfghjkl", "pokemon", "football", "killer", "112233", "michael", "shadow", "121212", "daniel", "asdasd", "qazwsx", "1234qwer", "superman", "123456a", "azerty", "qwe123", "master", "7777777", "sunshine", "N0=Acc3ss", "1q2w3e", "abcd1234", "1234561", "computer", "fuckyou ", "aaaaaa", "555555", "asdfgh", "asd123", "baseball", "0123456789", "charlie", "123654", "qwer1234", "naruto", "a123456", "jessica", "soccer", "jordan", "liverpool", "thomas", "lol123", "michelle", "123abc", "nicole", "11111111", "starwars", "samsung", "1111", "secret", "joshua", "123456789a", "andrew", "222222", "q1w2e3r4t5", "147258369", "hunter", "Password", "qazwsxedc", "lovely", "999999", "jennifer", "letmein", "tigger", "sunshine1", "hello123", "welcome1", "admin", "princess1", "password123", "football1", "letmein123", "iloveyou1", "monkey1", "12345678910", "1q2w3e4r5t6y", "123abc123", "superman1", "jackson1", "abcdefg", "12345a", "qwertyuiop123", "qwerty123456", "access123", "1111111", "dragon123", "michael1", "baseball1", "starwars1", "123qwe123", "jessica1", "babygirl1", "shadow1", "123456789abc", "trustno1", "iloveyou2", "harley1", "charlie1", "letmein1", "12345q", "mercedes1", "qwertyuio", "jordan23", "qwerty1234", "maggie1", "1234abcd", "123456b", "jennifer1", "justin1", "george1", "sunshine123", "ferrari1", "corvette1", "jackass1", "1qazxsw23edc", "qwerty12345", "ninja123", "snoopy1", "jason1", "dallas1", "michelle1", "marshall1", "p@ssw0rd" };
+
+        // Store Directory Paths
+        var directoryList = Directory.GetDirectories(GlobalClass.Global.applicationfolderpath, "*", SearchOption.AllDirectories);
+
+        // Skip the Folders for the list in Config
+        var configJson = JsonConvert.DeserializeObject<ConfigJson>(File.ReadAllText(GlobalClass.Global.configfilename));
+        var skipFolders = configJson.SkipFolders ?? Array.Empty<string>();
+        var updatedDirectoryList = directoryList.Except(skipFolders.Select(folder => Path.Combine(GlobalClass.Global.applicationfolderpath, folder))).ToArray();
+
+        // Create Counters for the SecurityPage
+        int riskyPasswordsCount = 0;
+        int weakPasswordsCount = 0;
+        int reusedPasswordsCount = 0;
+
+        // Iterate each Directory
+        foreach (var directory in updatedDirectoryList)
         {
-            list0.Remove(System.IO.Path.Combine(Global.applicationfolderpath, folder));
+            var directoryPath = Path.Combine(GlobalClass.Global.applicationfolderpath, directory);
+
+            // Store the Directory's files
+            var fileList = Directory.GetFiles(directoryPath, "*");
+
+            // Iterate each file 
+            foreach (var filePath in fileList)
+            {
+                // Check if the Format is JSON
+                if (File.ReadLines(filePath).Count() < 4)
+                {
+                    UpdateClass.FormatFileIdToJson(filePath);
+                }
+
+                // Decrypt password
+                var key = Application.Current.Properties["Key"].ToString();
+                var idJson = JsonConvert.DeserializeObject<UpdateClass.IdProperties>(File.ReadAllText(filePath));
+                var passwordDecrypted = CryptingClass.Aes256CbcEncrypter.Decrypt(idJson.Data[2], key);
+
+                var fileInfo = new FileInfo(filePath);
+                var fileName = fileInfo.Name.Replace(".aes", "");
+
+                // Check password's Security Level
+                if (worstPasswords.Contains(passwordDecrypted) || passwordDecrypted.Length < 5)
+                {
+                    riskyPasswordsCount++;
+                }
+                else if (passwordDecrypted.Length < 11 && (passwordDecrypted.All(char.IsLetter) || passwordDecrypted.All(char.IsNumber) || passwordDecrypted.All(char.IsLetterOrDigit)))
+                {
+                    weakPasswordsCount++;
+                }
+                else if (passwordList.Contains(passwordDecrypted))
+                {
+                    reusedPasswordsCount++;
+                }
+
+                // Add the password to the password's List
+                passwordList.Add(passwordDecrypted);
+            }
         }
-    }
 
-    // Update the list of Directory Paths
-    string[] updateddirlist = list0.ToArray();
+        // Calculate Safe password count
+        var safePasswordsCount = passwordList.Count - riskyPasswordsCount - weakPasswordsCount - reusedPasswordsCount;
 
+        RiskNum = riskyPasswordsCount.ToString();
+        WeakNum = weakPasswordsCount.ToString();
+        ReusedNum = reusedPasswordsCount.ToString();
+        SafeNum = safePasswordsCount.ToString();
+        SkippedNum = (ItemsTotal - passwordList.Count).ToString();
+        CheckedNum = (passwordList.Count).ToString();
 
-    // Iterate each Directory
-    foreach (string dir in updateddirlist)
-    {
-        string listpath = System.IO.Path.Combine(Global.applicationfolderpath, dir);
-        
-        // Store the Directory's files
-        string[] filelist = Directory.GetFiles(listpath, "*");
-
-        // Iterate each file 
-        foreach (string file in filelist)
+        // Calculate Security Rate
+        float securityRate;
+        if (passwordList.Count > 0)
         {
-            // Check if the Format is JSON
-            string[] lines = File.ReadAllLines(file);
-            if (lines.Length < 4)
-            {
-                UpdateClass.FormatFileIdToJson(file);
-            }
-
-            // Decrypt password
-            string Key = Application.Current.Properties["Key"].ToString();
-            string json1 = File.ReadAllText(file).ToString();
-            UpdateClass.IdProperties idJson = JsonConvert.DeserializeObject<UpdateClass.IdProperties>(json1);
-            string passwordDecrypted = CryptFunction.Decrypt(idJson.Data[2], Key);
-            
-            FileInfo fileinfo = new(file);
-            string filename = fileinfo.Name.Replace(".aes", "");
-            
-            // Check password's Security Level
-            if (worstpasswords.Contains(passwordDecrypted))
-            {
-                riskypasswordscount += 1;
-            }
-            else if (passwordDecrypted.Length < 8 || passwordDecrypted.All(char.IsLetter) || passwordDecrypted.All(char.IsNumber))
-            {
-                weakpasswordscount += 1;
-            }
-            else if (passwordlist.Count > 0 && passwordlist.Contains(passwordDecrypted))
-            {
-                reusedpasswordscount += 1;
-            }
-
-            // Add the password to the password's List
-            passwordlist.Add(passwordDecrypted);
+            securityRate = ((float)safePasswordsCount / passwordList.Count) * 100f;
+            securityRate = (float)Math.Round(securityRate * 100f) / 100f;
         }
-    }
+        else
+        {
+            securityRate = 100;
+        }
 
-    // Calculate Safe password count
-    safepasswordscount = passwordlist.Count - riskypasswordscount - weakpasswordscount - reusedpasswordscount;
+        SecurityRateNum = securityRate;
 
-    // Calculate Security Rate
-    float securityRate;
-    float safepasswordscountf = (float)safepasswordscount;
-    float passwordlistcountf = (float)passwordlist.Count;
-    if (passwordlist.Count > 0)
-    {
-        securityRate = (safepasswordscountf / passwordlistcountf) * 100f;
-        securityRate = (float)Math.Round(securityRate * 100f) / 100f;
+        // Return the string with Security Rate
+        return $"{securityRate:0}%";
     }
-    else
-    {
-        securityRate = 100;
-    }
-
-    // Return the string with Security Rate
-    return String.Format("{0:0}", securityRate) + "%";
 }
